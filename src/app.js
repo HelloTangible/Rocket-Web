f4774d60f42609db03b5ef22beb7b2d1e74b9ad8
Brandon Satrom <bsatrom@gmail.com>
2017-03-10 16:27:42 -0600

    added Auth0 support

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import './app.css'
+
+import App from 'containers/App/App'
+
+import {browserHistory} from 'react-router'
+import makeRoutes from './routes'
+
+const routes = makeRoutes()
+
+const Root = () => {
+  return (
+    <App history={browserHistory} routes={routes} />
+  )
+}
+
+render(<Root />, document.querySelector('#root'))
