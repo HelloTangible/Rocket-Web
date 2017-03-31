6f78acc18668049b31718072bc5a3fa5ab654fbd
Brandon Satrom <bsatrom@gmail.com>
2017-03-31 12:30:03 -0500

    added profile details back to AuthService

--- utils/AuthService.js
+++ utils/AuthService.js
@@ -1,7 +1,6 @@
-/* global localStorage, Auth0Lock */
+/* global localStorage, Auth0Lock, ROCKET_API_HOST */
 import Router from 'next/router'
 import Axios from 'axios'
-import { EventEmitter } from 'events'
 import { isTokenExpired } from './jwtHelper'
 
 export default class AuthService {
@@ -12,10 +11,18 @@ export default class AuthService {
     this.lock = new Auth0Lock(clientId, domain, {})
     this.lock.on('authenticated', this._doAuthentication.bind(this))
     this.login = this.login.bind(this)
+    this.loggedIn = this.loggedIn.bind(this)
   }
 
   _doAuthentication (authResult) {
     this.setToken(authResult.idToken)
+
+    this.lock.getProfile(authResult.idToken, (err, profile) => {
+      if (err) return console.log(`Error loading the Profile: ${err}`)
+
+      this.setProfile(profile)
+      this.saveIfNew(profile)
+    })
 
     Router.push('/')
   }
@@ -34,6 +41,34 @@ export default class AuthService {
     return (!!token && !isTokenExpired(token))
   }
 
+  setProfile (profile) {
+    localStorage.setItem('profile', JSON.stringify(profile))
+  }
+
+  getProfile () {
+    if (typeof localStorage === 'undefined') return
+
+    const profile = localStorage.getItem('profile')
+    return profile ? JSON.parse(localStorage.profile) : {}
+  }
+
+  saveIfNew (profile) {
+    // If this is a new user, save them to our internal DB
+    Axios.get(`http://${ROCKET_API_HOST}/api/users/${profile.user_id}`).then((res) => {
+      console.log(res)
+    }).catch(() => {
+      let user = {
+        user_id: profile.user_id,
+        email: profile.email,
+        picture: profile.picture,
+        email_verified: profile.email_verified,
+        name: profile.name
+      }
+
+      Axios.post(`http://${ROCKET_API_HOST}/api/users`, user)
+    })
+  }
+
   setToken (idToken) {
     localStorage.setItem('id_token', idToken)
   }
@@ -44,7 +79,7 @@ export default class AuthService {
 
   logout () {
     localStorage.removeItem('id_token')
-    
+    localStorage.removeItem('profile')
     Router.push('/login')
   }
 }
