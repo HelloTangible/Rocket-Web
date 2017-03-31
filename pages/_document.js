028dedfd686297b1ed6a5b3a7afea36198790e2d
Brandon Satrom <bsatrom@gmail.com>
2017-03-31 15:57:39 -0500

    added rebass and styled app header

--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,14 @@ import React from 'react'
 import Document, { Head, Main, NextScript } from 'next/document'
 import flush from 'styled-jsx/server'
 
+const layoutStyle = {
+  fontFamily: 'Open Sans, sans-serif',
+  color: '#111',
+  backgroundColor: '#fff',
+  lineHeight: '1.5',
+  margin: '0'
+}
+
 export default class MyDocument extends Document {
   static getInitialProps ({ renderPage }) {
     const {html, head} = renderPage()
@@ -11,12 +19,17 @@ export default class MyDocument extends Document {
 
   render () {
     return (
-      <html>
+      <html style={layoutStyle}>
         <Head>
           <script src='https://cdn.auth0.com/js/lock/10.5/lock.min.js'></script>
           <style>{`body { margin: 0 } /* custom! */`}</style>
         </Head>
         <body>
+          <style jsx global>{`
+            h1 {
+              font-size: 1.5em;
+            }
+          `}</style>
           <Main />
           <NextScript />
         </body>
